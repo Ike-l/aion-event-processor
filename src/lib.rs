e6f13f835c9a2b4d3a7e7b56441a48433ff252e4
isaac <isaacloff@gmail.com>
2026-05-07 23:23:17 +0100

    feature flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,29 @@
 pub mod get_mut_system_metadata;
 
+#[cfg(feature = "blocking-processor")]
+pub mod blocking_processor;
+
+#[cfg(feature = "non-blocking-processor")]
+pub mod non_blocking_processor;
+
 pub mod prelude {
     pub use super::{
         get_mut_system_metadata::{
             get_mut_system_metadata
+        }
+    };
+
+    #[cfg(feature = "blocking-processor")]
+    pub use super::{
+        blocking_processor::{
+
+        }
+    };
+
+    #[cfg(feature = "non-blocking-processor")]
+    pub use super::{
+        non_blocking_processor::{
+
         }
     };
 }
