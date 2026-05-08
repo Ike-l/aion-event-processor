85ad37bb028dcffa70529ffac0581918d9ad9aba
isaac <isaacloff@gmail.com>
2026-05-08 22:14:16 +0100

    get_mut_system_metadata by program id

--- src/get_mut_system_metadata/mod.rs
+++ src/get_mut_system_metadata/mod.rs
@@ -1,14 +1,16 @@
 use std::sync::Arc;
 
 use aion_processor::prelude::Unique;
-use aion_program::prelude::{AccessBuilder, AccessSubmissionError, ProgramRegistry, ResolveResourceError, ResourceId};
+use aion_program::prelude::{AccessBuilder, AccessSubmissionError, ProgramId, ProgramRegistry, ResolveResourceError, ResourceId};
 use aion_system::prelude::StoredSystemMetadata;
 
 pub fn get_mut_system_metadata<'a>(
     program_registry: &'a Arc<ProgramRegistry>,
-    system_metadata_resource_id: ResourceId
+    program_id: Option<ProgramId>,
+    system_metadata_resource_id: ResourceId,
 ) -> Result<Result<Unique<'a, StoredSystemMetadata>, ResolveResourceError>, AccessSubmissionError> {
     let access_builder = AccessBuilder {
+        program_id,
         resource_id: Some(system_metadata_resource_id),
         ..Default::default()
     };
